7f8c8470f86ecbc3776bfb068e4505b33c2f1c38
geauser <geauser@protonmail.com>
2022-07-07 08:48:28 +0200

    feat: add Icon component

---
+++ lib/components/Icon/Icon.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+
+defineProps<{
+  name?: string;
+}>();
+</script>
+<template>
+
+  <Icon
+    class="pointer-events-none transition-colors box-content"
+    aria-hidden="true"
+    :icon="name"
+  />
+
+</template>
